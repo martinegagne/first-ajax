edb7f1b8ff23b1cb6939fd31e7fe532aec0226b1
Martine Gagne <martine.e.gagne@gmail.com>
2017-08-21 15:56:44 -0400

    set up first AJAX request, bind click event

--- ajax.js
+++ ajax.js
@@ -1,5 +1,19 @@
 document.addEventListener("DOMContentLoaded", function() {
+  console.log("DOM CONTENT LOADED");
 
-  /* Your code goes here */
+  var button = document.getElementById('get_request_to_root_path')
+
+  button.addEventListener( 'click' , function() {
+    var request = $.ajax({
+    url: "http://first-ajax-api.herokuapp.com/",
+    method: 'GET',
+    data: '{}',
+    dataType: 'text'
+  });
+    request.done(function(data) {
+      console.log('REQUEST SUCCEEDED');
+      console.log( 'CLICKED BUTTON' );
+    });
+  });
 
 });
